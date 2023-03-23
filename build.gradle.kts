9b68d049def032204ef9b26d6f7977a2272fe069
nuraddin2000 <nuraddin.abdullayev32@gmail.com>
2023-03-23 09:20:56 +0400

    added some icons

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ buildscript {
         classpath(Dependencies.navigationSafeArgsPlugin)
         classpath(Dependencies.hiltAndroidGradlePlugin)
         classpath(Dependencies.kotlinSerializationClassPath)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
